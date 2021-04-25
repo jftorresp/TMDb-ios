acc3c0afa8959cc5f646de15560fc9e023da8f3d
Juan Felipe Torres <jf.torresp@outlook.com>
2021-04-25 10:49:49 -0500

    :boom::tada: Pagination done. Loading events and internet connection handled.

--- TMDb-ios/Presenters/MovieListPresenter.swift
+++ TMDb-ios/Presenters/MovieListPresenter.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 import Kingfisher
+import SystemConfiguration
 
 protocol MoviePresenterDelegate: AnyObject {
     func presentMovies(movies: [Movie])
@@ -19,26 +20,30 @@ class MoviePresenter {
     
     weak var delegate: PresenterDelegate?
     
-    public func getMovies() {
-        guard let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=56ac45a74cc38d542dac4bbbf3ca7eb8&language=en-US") else { return }
+    public func getMovies(page: Int, viewController: UIViewController) {
+        guard let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=56ac45a74cc38d542dac4bbbf3ca7eb8&language=en-US&page=\(page)") else { return }
         
         let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
             guard let data = data, error == nil else {
                 return
             }
-                        
+            
             do {
+                viewController.removeSpinner()
                 let response = try JSONDecoder().decode((DataMovies).self, from: data)
                 self?.delegate?.presentMovies(movies: response.results)
                 
             }
             catch {
                 print(error)
+                DispatchQueue.main.async {
+                    self!.showAlert(viewController: viewController)
+                }
             }
         }
         task.resume()
     }
-        
+    
     public func setViewDelegate(delegate: PresenterDelegate) {
         
         self.delegate = delegate
@@ -57,4 +62,35 @@ class MoviePresenter {
         movieDetailVC.movie = movie
         nav.pushViewController(movieDetailVC, animated: true)
     }
+    
+    public func isInternetAvailable() -> Bool
+    {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        return (isReachable && !needsConnection)
+    }
+    
+    func showAlert(viewController: UIViewController) {
+        if !isInternetAvailable() {
+            let alert = UIAlertController(title: "Warning", message: "Problem with internet connectivity or server, please try after some time", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Dismiss", style: .default, handler: nil)
+            alert.addAction(action)
+            viewController.present(alert, animated: true, completion: nil)
+        }
+    }
+    
 }
--- TMDb-ios/Views/MoviesViewController.swift
+++ TMDb-ios/Views/MoviesViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 import Kingfisher
 
-class MoviesViewController: UIViewController, MoviePresenterDelegate {
-
+class MoviesViewController: UIViewController, MoviePresenterDelegate, UIScrollViewDelegate {
+    
     @IBOutlet weak var moviesCollectionView: UICollectionView!
     
     private var movies = [Movie]()
     private let presenter = MoviePresenter()
+    var page: Int = 1
+    var isPageRefreshing:Bool = false
+    var dataFetched: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,18 +28,23 @@ class MoviesViewController: UIViewController, MoviePresenterDelegate {
         
         moviesCollectionView.register(UINib(nibName: "MovieCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "movieCollectionViewCell")
         
+        // The spinner will show if the data hasn't been fetched yet
+        showSpinner(onView: self.view)
         // Presenter
         presenter.setViewDelegate(delegate: self)
-        presenter.getMovies()
-                
+        presenter.getMovies(page: self.page, viewController: self)
+        
     }
     
     //MARK: - Presenter Delegate Methods
     
     func presentMovies(movies: [Movie]) {
         
-        self.movies = movies
-                
+        dataFetched = true
+        
+        self.movies = self.movies + movies
+        
+        // Update poster_path url for all images adding the base URL
         for i in 0..<self.movies.count{
             self.movies[i].poster_path = "http://image.tmdb.org/t/p/w342" + self.movies[i].poster_path
         }
@@ -45,6 +53,20 @@ class MoviesViewController: UIViewController, MoviePresenterDelegate {
             self.moviesCollectionView.reloadData()
         }
     }
+    
+    //MARK: - ScrollView Handler for Pagination
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+       if(self.moviesCollectionView.contentOffset.y >= (self.moviesCollectionView.contentSize.height - self.moviesCollectionView.bounds.size.height)) {
+           if !isPageRefreshing {
+               isPageRefreshing = true
+               page = page + 1
+               presenter.getMovies(page: page, viewController: self)
+           }
+        
+        isPageRefreshing = false
+       }
+   }
 }
 
 //MARK: - UICollectionViewDelegate Methods
@@ -56,7 +78,6 @@ extension MoviesViewController: UICollectionViewDelegate, UICollectionViewDelega
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "movieCollectionViewCell", for: indexPath as IndexPath) as! MovieCollectionViewCell
-                
         let movie = movies[indexPath.row]
         
         cell.movieTitle.text = movie.title
@@ -73,7 +94,6 @@ extension MoviesViewController: UICollectionViewDelegate, UICollectionViewDelega
         
         let dateString = dateFormatterDateToStrIng.string(from: date)
         cell.movieReleaseDate.text = dateString
-        
         return cell
     }
     
@@ -81,10 +101,10 @@ extension MoviesViewController: UICollectionViewDelegate, UICollectionViewDelega
     {
         return CGSize(width: (view.frame.width / 2) - 5, height: 292)
     }
-    
+        
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         // Ask presenter to handle the tap
         presenter.didTap(movie: movies[indexPath.row], nav: self.navigationController!)
     }
-
+    
 }
